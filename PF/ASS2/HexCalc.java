72ddaccae0b1d5c7ed2838efc31c1e8bda0786c2
meta-rahul-dosi <52689655+meta-rahul-dosi@users.noreply.github.com>
2019-07-15 17:51:41 +0530

    Update HexCalc.java

--- PF/ASS2/HexCalc.java
+++ PF/ASS2/HexCalc.java
@@ -14,12 +14,12 @@ public class HexCalc
   h1 = input.nextLine();
   h2 = input.nextLine();
  
-  if(!h1.matches(pattern)) 
+  if(!h1.matches(pattern))
   System.out.println("invalid first input");
   if(!h2.matches(pattern))
   System.out.println("invalid second input");	  
   
-  System.out.println("enter 1 for addition, 2 for subtraction, 3 for multiplication, 4 for division, 5 for hex2dec conversion, 6 for dec2hex conversion");
+  System.out.println("enter 1 for addition\n2 for subtraction\n3 for multiplication\n4 for division\n5 for hex2dec conversion\n6 for dec2hex conversion\n7 for comparing both strings");
   choice = input.nextInt();
   hex2dec(h1,h2,choice);
  } 
@@ -65,16 +65,35 @@ public class HexCalc
  	  case 2 : sub(dec1,dec2); break;
  	  case 3 : mul(dec1,dec2); break;
  	  case 4 : div(dec1,dec2); break;
- 	  case 5 : 
+ 	  case 5 : h2d(h1,h2); break;
  	  case 6 : dec2hex(dec1,dec2); break;
+ 	  case 7 : compare(h1,h2); break;
  	  default: System.out.println("\nyour game is over");
  	  }
   }
   
+  static void h2d(String h1, String h2)
+  {
+	int dec1 = Integer.parseInt(h1,16); 
+    int dec2 = Integer.parseInt(h2,16);  
+    System.out.println(dec1+" and "+dec2);
+  }
+  
   static void dec2hex(int d1, int d2)
-  {  
-	int d = d1+d2;  
-	String s = Integer.toHexString(d);
-	System.out.println(s);
+  {    
+	String s1 = Integer.toHexString(d1);
+	String s2 = Integer.toHexString(d2);
+	System.out.println(s1+" and "+s2);
+  }
+  
+  static void compare(String h1, String h2)
+  {
+	int a = h1.compareTo(h2);
+	if(a==1)
+	System.out.println(h1+" is greater");
+	else if(a==-1)
+	System.out.println(h2+" is greater");
+	else if(a==0)
+	System.out.println(h1+" is equal to "+h2);
   }
 }
