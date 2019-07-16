b98692238de21a284c3c9119c3c6397c5c33c337
meta-rahul-dosi <rahul.dosi@metacube.com>
2019-07-16 19:19:12 +0530

    adding

---
+++ ASS/ASS3/shapes/area.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+class area {
+
+	public static void main(String[] args) {
+		area obj = new area();
+		System.out.println("enter width, height and radius");
+		Scanner inputParameters = new Scanner(System.in);
+		double width = inputParameters.nextDouble();
+		double height = inputParameters.nextDouble();
+		double radius = inputParameters.nextDouble();
+
+		System.out.println("circle area is " + obj.circle(radius));
+		System.out.println("triangle area is " + obj.triangle(width, height));
+		System.out.println("square area is " + obj.square(width));
+		System.out.println("rectangle area is " + obj.rectangle(width, height));
+		inputParameters.close();
+	}
+
+	double circle(double radius) {
+		return (3.14 * radius * radius);
+	}
+
+	double square(double width) {
+		return (width * width);
+	}
+
+	double rectangle(double width, double height) {
+		return (width * height);
+	}
+
+	double triangle(double width, double height) {
+		return (width * height / 2);
+	}
+
+}
