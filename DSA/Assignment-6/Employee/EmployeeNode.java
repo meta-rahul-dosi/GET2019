439b8553064175ed0c187b554aab9f9d5688bef3
meta-rahul-dosi <rahul.dosi@metacube.com>
2019-08-06 18:57:23 +0530

    dsa

---
+++ DSA/Assignment-6/Employee/EmployeeNode.java
@@ -0,0 +1,44 @@
+package Employee;
+
+public class EmployeeNode {
+
+	String name;
+	int id;
+	int age;
+	float salary;
+
+	// getters and setters
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public float getSalary() {
+		return salary;
+	}
+
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
+
+}
